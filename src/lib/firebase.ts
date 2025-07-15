d45127ab72335f2b7d7f6b1e67291e5f7039a3ae
Vyom <vyomsfly@gmail.com>
2025-07-14 18:03:28 -0700

    Initial commit: FinMate financial education app with secure configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAG_kjN1BVgiIenlkyEAy1tbp-a5VFdkBI",
-  authDomain: "finmate-b3b55.firebaseapp.com",
-  projectId: "finmate-b3b55",
-  storageBucket: "finmate-b3b55.firebasestorage.app",
-  messagingSenderId: "441224019586",
-  appId: "1:441224019586:web:5ba271c7f0befb8828be9b",
-  measurementId: "G-738WPMY45B"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
